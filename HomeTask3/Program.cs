808a3bfb5e0240c7f94dfb1cdc8f8133862edaaf
Oleg Dobrovolskikh <baevar@mail.ru>
2024-04-07 02:01:33 +0700

    Решил hometask3

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+
+void ReverseArray(int[] array, int count)       // рекурсивная функция для вывода массива в обтратном порядке
+{
+
+    if (array.Length == count)
+    {
+        return;
+    }
+    count++;
+
+    ReverseArray(array, count);
+
+    Console.Write(array[count - 1] + " ");
+
+}
+
+void PrintArray(int[] array)            // Вывод созданного массива на консоль
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i + 1 == array.Length)
+        {
+            Console.Write(array[i]);
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]");
+}
+
+int[] CreateRandomArray(int size)       // Создаю массив из случайных чисел
+
+{
+    Random rnd = new Random();
+
+    int[] arr = new int[size];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 11);
+    }
+    return arr;
+}
+
+bool IsNaturalNumb(int numb)        // проверка, удовлетворяет ли введенное число условиям создания размера массива
+{
+    if (numb < 1)
+    {
+        Console.WriteLine("Некорректный ввод");
+        return false;
+    }
+    return true;
+}
+
+Console.Write("Введите размер массива = ");
+
+if (!Int32.TryParse(Console.ReadLine(), out int size))     //проверяю, является ли введенная строка числом
+{
+    Console.WriteLine("Некорректный ввод");
+
+    return;
+}
+
+if (!IsNaturalNumb(size)) return;
+
+int[] array = CreateRandomArray(size);
+
+Console.WriteLine("Исходный массив:");
+
+
+PrintArray(array);
+
+Console.WriteLine();
+
+Console.WriteLine("Результат вывода");
+
+int count = 0;
+ReverseArray(array, count);
