720a5d349de7b89751de1dec9d83a818791a8c10
Oleg Dobrovolskikh <baevar@mail.ru>
2024-04-07 02:01:55 +0700

    решение заданий с семинара

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// Указание
+// Использовать рекурсию.
+// Пример
+// 123 => 6
+// 63 => 9
+
+int SumDigitInNum(int numb)
+{
+    if (numb == 0)
+    {
+        return 0;
+    }
+
+    return (numb % 10) + SumDigitInNum(numb / 10);
+}
+
+Console.WriteLine("Введите число");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDigitInNum(numb);
+
+Console.WriteLine($"Симма цифр числа={result}");
---
+++ Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Считать строку с консоли, содержащую латинские буквы. Вывести на экран согласные буквы этой строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l “World” => W r l d 
+
+// void PrintConsonants(string str, int count)
+// {
+//     if (count == str.Length) return;
+//     string vowels = "aouiey";
+//     if(char.IsAsciiLetter(str[count]) && !vowels.Contains(char.ToLower(str[count])))
+//     {
+//         Console.Write(str[count] + " ");
+//     }
+
+//     PrintConsonants(str, count + 1);
+// }
+
+
+// Console.WriteLine("Введите строку латинскими буквами");
+// string userInput = Console.ReadLine();
+// int posStr = 0;
+// PrintConsonants(userInput, posStr);
+
+void PrintConsonants(string str)
+{
+    
+        
+    if (str.Length == 0) return;
+    string vowels = "aouiey";
+    if (char.IsAsciiLetter(str[0]) && !vowels.Contains(char.ToLower(str[0])))
+    {
+        Console.Write(str[0] + " ");
+    }
+
+    PrintConsonants(str.Substring(1));
+}
+
+
+Console.WriteLine("Введите строку латинскими буквами");
+string userInput = Console.ReadLine();
+int posStr = 0;
+PrintConsonants(userInput);
